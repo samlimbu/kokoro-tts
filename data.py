14adbe031ce1ea130f34a1dc2d32bcdcf26f7e63
samlimbu <33290648+samlimbu@users.noreply.github.com>
2025-05-27 17:33:17 +0545

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 chapterLIST = [
     
-{'title':'c1', 'content':[{'slide':'c1s1_1','text':"The history of money laundering is as old as illicit activities themselves, but the methods and sophistication have evolved significantly over time."}]}
+{'title':'c1', 'content':[{'slide':'c1s1_1','text':"The history of money laundering traces back to over 2000 years ago when wealthy Chinese merchants started to ‘clean’ their profits as a way to circumvent regional trading bans. In the prohibition era (1920s) in the United States, when organized crime syndicates laundered illicit alcohol sales profits through seemingly legitimate businesses."}]}
 
 ]
