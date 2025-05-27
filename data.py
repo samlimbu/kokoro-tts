57773085da6e7540cec9ca7e661dc172c7d4e46a
sam <samlimbu@hotmail.com>
2025-05-27 09:35:14 +0545

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 chapterLIST = [
     
-{'title':'temp', 'content':[{'slide':'temp','text':"F A, T F's . countries F A. T F. listed in F A, T Fs" }]},
+{'title':'c1', 'content':[{'slide':'c7_6','text':"As Nepal is member of APG, one of the FSRBs, M, E of Nepal is conducted by APG."}]},
+
 
 
 
