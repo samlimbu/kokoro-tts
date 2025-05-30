5191f48957a49c7661e08b6aabb2bf50b1210d21
samlimbu <33290648+samlimbu@users.noreply.github.com>
2025-05-30 14:21:15 +0545

    530

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 chapterLIST = [
 
-{'title':'c1', 'content':[{'slide':'c1s17_11','text':"4. If non-compliance of provision of Chapter 6B of the Act has been found. Then For first time, impose a fine of Nepali Rupees 2 million. For second time, impose a fine of Nepali Rupees 5 million. From third time onwards, impose a fine of Nepali Rupees 20 million or up toRupees50 million as per severity. If there are frequent systemic problems, take action of partial or complete suspension or cancellation of the license."}]},
+{'title':'c1', 'content':[{'slide':'c1s17_26','text':"15. Money Laundering Prevention Committee did not present report on time and Board of Directors did not evaluate , then, Ask clarification for first time; then, For second time, impose a fine of Rupees 1 million From third time onwards. impose a fine of Nepali Rupees  5 million or higher as per severity"}]}
+
 
 ]
