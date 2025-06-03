b13f86540f366b3ea9eacc4828f3a12793f61d31
samlimbu <33290648+samlimbu@users.noreply.github.com>
2025-06-03 17:36:54 +0545

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 chapterLIST = [
 
-{'title':'c1', 'content':[{'slide':'c1s4_5','text':"CASE Study on Terrorist Financing;-,;-… Nepal’s first TF case;-,;-,;-… TF Convictions;-,;-,;-… Nepal’s first TF case to reach final judgement resulted in seven persons convicted of TF;-,;-,;-… one as a principal offender and six as accomplices. The case was initiated in 2011 via international cooperation with a neighboring jurisdiction and involved the movement of funds from a foreign jurisdiction into Nepal."}]},
+{'title':'c1', 'content':[{'slide':'c2A_1','text':"Note that Recommendations marked with an asterisk have intpretive notes, which should be read in conjunction with the Recommendation"}]},
 
 
 ]
