8e828796f29fcddbf8ff2e3e27a9d87fdf06fbdc
samlimbu <33290648+samlimbu@users.noreply.github.com>
2025-06-22 16:59:01 +0545

    Update data.py

--- data.py
+++ data.py
@@ -1,7 +1,10 @@
 chapterLIST = [
 
-	{'title':'c', 'content':[{'slide':'demo','text':"You have now finished reading the course. You may now take the post-assessment for this course."}]},
-
+{'title':'c', 'content':[{'slide':'c1s1c_0_0','text':"Chapter 1 ;-,;-,;-… Money Laundering, Terrorist Financing and Proliferation Financing"}]},
+{'title':'c', 'content':[{'slide':'c2_h2_155_0_0','text':"Chapter 2;-,;-,;-… AML-CFT Related International Bodies"}]},
+{'title':'c', 'content':[{'slide':'c3h2_193_0','text':"Chapter 3;-,;-,;-… Regulatory Bodies and Prevailing Laws, Regulations in Nepal"}]},
+{'title':'c', 'content':[{'slide':'c3s5c_246_0','text':"Chapter 4;-,;-,;-… M.L. T.F, Supervision and Supervisory guidelines"}]},
+{'title':'c', 'content':[{'slide':'c7_289_0','text':"Chapter 7;-,;-,;-… Mutual Evaluation"}]}
 ]
 
 
