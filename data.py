ff7820f23e2c48d6d6e837a1b06faa26ec07b7b2
samlimbu <samlimbu@hotmail.com>
2025-08-26 18:06:19 +0545

    Update data.py

--- data.py
+++ data.py
@@ -1,10 +1,10 @@
 chapterLIST = [
 
-{'title':'c', 'content':[{'slide':'c1s1c_0_0','text':"Chapter 1 ;-,;-,;-… Money Laundering, Terrorist Financing and Proliferation Financing"}]},
-{'title':'c', 'content':[{'slide':'c2_h2_155_0_0','text':"Chapter 2;-,;-,;-… AML-CFT Related International Bodies"}]},
-{'title':'c', 'content':[{'slide':'c3h2_193_0','text':"Chapter 3;-,;-,;-… Regulatory Bodies and Prevailing Laws, Regulations in Nepal"}]},
-{'title':'c', 'content':[{'slide':'c3s5c_246_0','text':"Chapter 4;-,;-,;-… M.L. T.F, Supervision and Supervisory guidelines"}]},
-{'title':'c', 'content':[{'slide':'c7_289_0','text':"Chapter 7;-,;-,;-… Mutual Evaluation"}]}
+{'title':'demo1', 'content':[{'slide':'RB_1','text':"rb one ndering, Terrorist Financing and Proliferation Financing"}]},
+{'title':'demo1', 'content':[{'slide':'RB_2','text':"rb two 2;-,;-,;-… AML-CFT Related International Bodies"}]},
+{'title':'demo1', 'content':[{'slide':'RB_3','text':"rb three 3;-,;-,;-… Regulatory Bodies and Prevailing Laws, Regulations in Nepal"}]},
+{'title':'demo1', 'content':[{'slide':'RB_4','text':"rb four 4;-,;-,;-… M.L. T.F, Supervision and Supervisory guidelines"}]},
+{'title':'demo1', 'content':[{'slide':'RB_5','text':"rb five 7;-,;-,;-… Mutual Evaluation"}]}
 ]
 
 
