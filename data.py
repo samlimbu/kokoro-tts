93f2e417e415c1211847acbb0fa2edc43e28beaa
samlimbu <33290648+samlimbu@users.noreply.github.com>
2025-08-29 09:34:00 +0545

    Update data.py

--- data.py
+++ data.py
@@ -4,7 +4,11 @@ chapterLIST = [
 {'title':'demo1', 'content':[{'slide':'RB_2','text':"The objectives of this unit are to understand the:"}]},
 {'title':'demo1', 'content':[{'slide':'RB_3','text':"History and definition of Retail Banking"}]},
 {'title':'demo1', 'content':[{'slide':'RB_4','text':"Characteristics of Retail Banking"}]},
-{'title':'demo1', 'content':[{'slide':'RB_5','text':"Advantages and constraints of Retail Banking"}]}
+{'title':'demo1', 'content':[{'slide':'RB_5','text':"Advantages and constraints of Retail Banking"}]},
+{'title':'demo1', 'content':[{'slide':'RB_6','text':"Retail banking scenario in Nepal – Why Retail Banking? Factors for Success, Challenges, FutureStrategy"}]},
+{'title':'demo1', 'content':[{'slide':'RB_7','text':"Global trend in Retail Banking"}]},
+{'title':'demo1', 'content':[{'slide':'RB_8','text':"INTRODUCTION"}]},
+{'title':'demo1', 'content':[{'slide':'RB_9','text':"Retail Banking refers to provision of banking products and services offered to individual customers, typically for non-entrepreneurial purposes."}]}
 ]
 
 
