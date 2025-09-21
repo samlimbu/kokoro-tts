c13d539258b333ad708a964805f7873f2ac14344
samlimbu <33290648+samlimbu@users.noreply.github.com>
2025-09-21 17:31:05 +0545

    123

--- data.py
+++ data.py
@@ -1,14 +1,19 @@
 chapterLIST = [
-{'title':'demo1', 'content':[{'slide':'RB_0','text':"Unit 1: RETAIL BANKING: INTRODUCTION"}]},
-{'title':'demo1', 'content':[{'slide':'RB_1','text':"OBJECTIVES"}]},
-{'title':'demo1', 'content':[{'slide':'RB_2','text':"The objectives of this unit are to understand the:"}]},
-{'title':'demo1', 'content':[{'slide':'RB_3','text':"History and definition of Retail Banking"}]},
-{'title':'demo1', 'content':[{'slide':'RB_4','text':"Characteristics of Retail Banking"}]},
-{'title':'demo1', 'content':[{'slide':'RB_5','text':"Advantages and constraints of Retail Banking"}]},
-{'title':'demo1', 'content':[{'slide':'RB_6','text':"Retail banking scenario in Nepal – Why Retail Banking? Factors for Success, Challenges, FutureStrategy"}]},
-{'title':'demo1', 'content':[{'slide':'RB_7','text':"Global trend in Retail Banking"}]},
-{'title':'demo1', 'content':[{'slide':'RB_8','text':"INTRODUCTION"}]},
-{'title':'demo1', 'content':[{'slide':'RB_9','text':"Retail Banking refers to provision of banking products and services offered to individual customers, typically for non-entrepreneurial purposes."}]}
+
+
+{'title':'rb', 'content':[{'slide':'RB_0','text':"unit 1;-,;-,;-… retail banking;-,;-,;-… "}]},
+{'title':'rb', 'content':[{'slide':'RB_1','text':"objectives"}]},
+{'title':'rb', 'content':[{'slide':'RB_8','text':"introduction"}]},
+{'title':'rb', 'content':[{'slide':'RB_14','text':"definitions:"}]},
+{'title':'rb', 'content':[{'slide':'RB_22','text':"characteristics of retail banking"}]},
+{'title':'rb', 'content':[{'slide':'RB_31','text':"advantages of retail banking"}]},
+{'title':'rb', 'content':[{'slide':'RB_39','text':"constraints in retail banking"}]},
+{'title':'rb', 'content':[{'slide':'RB_47','text':"evolution of retail banking"}]},
+{'title':'rb', 'content':[{'slide':'RB_49','text':"retail banking as a business model"}]},
+{'title':'rb', 'content':[{'slide':'RB_59','text':"retail banking scenario in nepal"}]},
+{'title':'rb', 'content':[{'slide':'RB_65','text':"nrb data on retail banking in nepal"}]},
+{'title':'rb', 'content':[{'slide':'RB_89','text':"performance of different segments of retail banking"}]}
+
 ]
 
 
