ec538268fb03b4d94adcf364887f6f263d4e197a
samlimbu <33290648+samlimbu@users.noreply.github.com>
2025-09-28 11:19:08 +0545

    12

--- data.py
+++ data.py
@@ -1,12 +1,6 @@
 chapterLIST = [
 
-{'title':'rb', 'content':[{'fname':'RB_intro','text':"Welcome to National Banking Institute's Retail Banking course. Please Press right key or swipe up to begin or keep playing this audio narration to auto advance."}]},
-{'title':'rb', 'content':[{'fname':'RB_menu','text':"This are the buttons that will be available throughout the course interface. Please make note of it."}]},
-{'title':'rb', 'content':[{'fname':'RB_end','text':"You have now completed the course."}]},
-{'title':'rb', 'content':[{'fname':'RB_h_0_0','text':"unit 1;-,;-,;-… retail banking;-,;-,;-… "}]},
-{'title':'rb', 'content':[{'fname':'RB_h_1_1','text':"objectives"}]},
-{'title':'rb', 'content':[{'fname':'RB_h_1_2','text':"The objectives of this unit are to understand the:"}]},
-{'title':'rb', 'content':[{'fname':'RB_h_1_3','text':"History and definition of Retail Banking"}]}
+{'title':'rb', 'content':[{'fname':'RB_h_6_8','text':"Capgemini brings out World Retail Banking Reports from time to time. “The World Retail Banking Report 2015” was released by Capgemini and European Financial Management & Marketing Association(E. F. M. A.) The major findings were:"}]}
 
 ]
 
