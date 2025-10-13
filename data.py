e6e42e55c99ac54fbf8d409912ed1a6a22116fe5
samlimbu <samlimbu@hotmail.com>
2025-10-13 19:41:23 +0545

    Update data.py

--- data.py
+++ data.py
@@ -2,8 +2,14 @@ chapterLIST = [
 
 
 
-
-
+{'title':'rb', 'content':[{'fname':'RB_h_19_9','text':"Serial Number ;Need Level ;Matching Banking, Investment and Insurance Products ;"}]},
+{'title':'rb', 'content':[{'fname':'RB_h_57_37','text':"Serial Number ;Standalone Model (Branch Level Processing) ;Centralised Model ;(Retail Loan Processing Hubs/Centres) ;"}]},
+{'title':'rb', 'content':[{'fname':'RB_h_60_179','text':"Serial Number ;Type of facility/Penalty ;Charges ;"}]},
+{'title':'rb', 'content':[{'fname':'RB_h_63_34','text':"Serial Number ;Credit Cards ;Debit Cards ;"}]},
+{'title':'rb', 'content':[{'fname':'RB_h_86_3','text':"Various "}]},
+{'title':'rb', 'content':[{'fname':'RB_h_116_21','text':"The formula for the calculation of E. M. I. is:;-,;-… E. M. I. equals P multiplied by r multiplied by open bracket one plus r close bracket raised to the power n, divided by open bracket one plus r close bracket raised to the power n, minus one."}]},
+{'title':'rb', 'content':[{'fname':'RB_h_141_187','text':"Serial Number ;Account open date ;Account No ;Type of account ;Account Name ;Balance as on …. ;/…/…… ;Debit Card (Y/N) ;Credit Card ;(Y/N) ;Mobile Banking ;(Y/N) ;E-Banking (Y/N) ;Locker Service ;(Y/N) ;"}]},
+{'title':'rb', 'content':[{'fname':'RB_h_157_131','text':"Where F = future value of an annuity A = annuity i = interest rate"}]}
 
 
 
