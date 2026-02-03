0004fb68cfa00cc60080e97a68a9ffec3b91731d
samlimbu <33290648+samlimbu@users.noreply.github.com>
2026-02-03 20:22:38 +0545

    3feb26

--- data.py
+++ data.py
@@ -1,22 +1,8 @@
 chapterLIST = [
 
-{'title':'c', 'content':[{'fname':'c1s14_61_0_0','text':"Mechanism to immediately identify Pep on updated Pep list"}]},
-{'title':'c', 'content':[{'fname':'c1s14_61_0_1','text':"Mechanism to identify family members and close associates of Pep "}]},
-{'title':'c', 'content':[{'fname':'c1s17_70_0_1','text':"Note;-,;-,;-… If the actions to be taken according to these provisions are not sufficient, NRB may take additional actions against BF.I's in accordance with the Nepal Rastra Bank Act, 2058 and the Bank and Financial Institutions Act, 2073."}]},
-{'title':'c', 'content':[{'fname':'c1s18_76_0_1','text':"KYC has gained significance over the years as it helps to verify the identity of their customers, assess potential risks, prevent fraud, and comply with legal and regulatory requirements."}]},
-{'title':'c', 'content':[{'fname':'c1s18_80_0','text':"g. Record-Keeping:;-,;-… Time period to retain records of customer identification and transaction records for a specified period.   "}]},
-{'title':'c', 'content':[{'fname':'c1s25_112_0','text':"Based on the STRs reported by the bank, FIU-Nepal conducted thorough analysis and revealed following facts;-,;-,;-… There were many transactions in Mr. A's bank statement with description Payoneer card, Payoneer Exchange, P.E., Payo $100 etc. Mr. Bees amount is also found deposited in Mr. A's account. There may be some other issues which leads Mr. A to refuse to load Mr. Bee's Payoneer card."}]},
-{'title':'c', 'content':[{'fname':'c1s25_115_0_0','text':"Online betting site like 1xbet; Fifiya; Metabet etc. are operating in Nepal and is been advertised through social media. This companies hire people who provide their personal account for certain commission. The account is advertised or shown for certain time in the platform to receive the deposit.  "}]},
-{'title':'c', 'content':[{'fname':'c1s25_117_0_0','text':"S.T. AR's, S, A, AR's are on rise where people have actually purchased the virtual assets using Nepalese account."}]},
-{'title':'c', 'content':[{'fname':'c1s25_124_0_0','text':"Mr B was high level official of a public entity. He has purchased many IT related component. Even one of supplier's partner was his close relative. The proceeds of crime were settlement in foreign Jurisdiction."}]},
-{'title':'c', 'content':[{'fname':'c1s25_129_0_2','text':"Mobile no. 98****** appeared on 1xbet site. Further, mobile number was verified and observed that this mobile number was related with B***** Shoes Collection which is a proprietorship firm in the number of Mr. S. Mr. S has been arrested by Nepal Police for betting related offence."}]},
-{'title':'c', 'content':[{'fname':'c1s25_132_0_3','text':"During the investigation process it was revealed that people deposit the cash in the PSP account and that slip was sent to the receiver to get the cash from it's agent network."}]},
-{'title':'c', 'content':[{'fname':'c1s26_133_0_0','text':"Beneficial owner- it refers to the natural person(s) who ultimately owns or controls or holds management position with respect to legal person or arrangement."}]},
-{'title':'c', 'content':[{'fname':'c1s26_133_0_2','text':"Compliance Officer- is an employee (of at least management level) responsible for ensuring that the organization adheres to legal and regulatory requirements, industry standards, and internal policies related with AML CFT."}]},
-{'title':'c', 'content':[{'fname':'c1s26_138_0_1','text':"Review;-,;-,;-… Finally, the review step involves the ongoing evaluation of the effectiveness of the TMS. This includes reviewing the parameters and alerts to ensure they are still relevant and practical and reviewing any false positives or negatives to identify areas for improvement."}]},
-{'title':'c', 'content':[{'fname':'c2_159_0_0','text':"Note that Recommendations marked with an asterisk have interpretive notes, which should be read in conjunction with the Recommendation"}]},
-{'title':'c', 'content':[{'fname':'c5_260_0_1','text':"To ensure this NRB has developed questionnaire to assess the adequacy of their policies and internal controls for deterring, detecting and reporting suspected money laundering and terrorist financing activities (M.L. F.T,)."}]},
-{'title':'c', 'content':[{'fname':'c5_265_0_0','text':"The goAML application is the Software Products developed by United Nations Office on Drugs and Crime U N O D C in order to respond to money laundering and terrorist financing, and Financial Intelligence Units bolstering their efforts in combating financial crimes."}]},
+{'title':'c', 'content':[{'fname':'c3_197_0_1','text':"Regulator;-,;-,;-… Department of Co-operatives, Province Governments and Local Governments"}]},
+{'title':'c', 'content':[{'fname':'c3_197_0_2','text':"Reporting Entities;-,;-,;-… Co-operatives"}]},
+{'title':'c', 'content':[{'fname':'c7_308_0_1','text':"Currently there are three countries in black list viz."}]}
 
 ]
 
